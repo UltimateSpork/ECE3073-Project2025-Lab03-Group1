46fc11b86845af368289f7e273ba9d295acfe0e2
UltimateSpork <shahmir2003@outlook.com>
2026-04-21 15:45:38 +0800

    Testing

--- Quartus/M1/M1_bb.v
+++ Quartus/M1/M1_bb.v
@@ -1,5 +1,6 @@
 
 module M1 (
+	camera_ready_export,
 	clk_clk,
 	hex0_export,
 	hex1_export,
@@ -7,9 +8,12 @@ module M1 (
 	hex3_export,
 	hex4_export,
 	hex5_export,
+	img_addr_export,
 	key0_export,
 	key1_export,
 	leds_export,
+	pio_wren_export,
+	pixel_export,
 	reset_reset_n,
 	sdram_addr,
 	sdram_ba,
@@ -21,11 +25,12 @@ module M1 (
 	sdram_ras_n,
 	sdram_we_n,
 	switches_export,
-	pixel_export,
-	camera_ready_export,
-	img_addr_export,
-	pio_wren_export);	
+	accel_I2C_SDAT,
+	accel_I2C_SCLK,
+	accel_G_SENSOR_CS_N,
+	accel_G_SENSOR_INT);	
 
+	output		camera_ready_export;
 	input		clk_clk;
 	output	[7:0]	hex0_export;
 	output	[7:0]	hex1_export;
@@ -33,9 +38,12 @@ module M1 (
 	output	[7:0]	hex3_export;
 	output	[7:0]	hex4_export;
 	output	[7:0]	hex5_export;
+	output	[16:0]	img_addr_export;
 	input		key0_export;
 	input		key1_export;
 	output	[8:0]	leds_export;
+	output		pio_wren_export;
+	output	[3:0]	pixel_export;
 	input		reset_reset_n;
 	output	[12:0]	sdram_addr;
 	output	[1:0]	sdram_ba;
@@ -47,8 +55,8 @@ module M1 (
 	output		sdram_ras_n;
 	output		sdram_we_n;
 	input	[3:0]	switches_export;
-	output	[3:0]	pixel_export;
-	output		camera_ready_export;
-	output	[16:0]	img_addr_export;
-	output		pio_wren_export;
+	inout		accel_I2C_SDAT;
+	output		accel_I2C_SCLK;
+	output		accel_G_SENSOR_CS_N;
+	input		accel_G_SENSOR_INT;
 endmodule
--- Quartus/M1/M1_inst.v
+++ Quartus/M1/M1_inst.v
@@ -1,4 +1,5 @@
 	M1 u0 (
+		.camera_ready_export (<connected-to-camera_ready_export>), // camera_ready.export
 		.clk_clk             (<connected-to-clk_clk>),             //          clk.clk
 		.hex0_export         (<connected-to-hex0_export>),         //         hex0.export
 		.hex1_export         (<connected-to-hex1_export>),         //         hex1.export
@@ -6,9 +7,12 @@
 		.hex3_export         (<connected-to-hex3_export>),         //         hex3.export
 		.hex4_export         (<connected-to-hex4_export>),         //         hex4.export
 		.hex5_export         (<connected-to-hex5_export>),         //         hex5.export
+		.img_addr_export     (<connected-to-img_addr_export>),     //     img_addr.export
 		.key0_export         (<connected-to-key0_export>),         //         key0.export
 		.key1_export         (<connected-to-key1_export>),         //         key1.export
 		.leds_export         (<connected-to-leds_export>),         //         leds.export
+		.pio_wren_export     (<connected-to-pio_wren_export>),     //     pio_wren.export
+		.pixel_export        (<connected-to-pixel_export>),        //        pixel.export
 		.reset_reset_n       (<connected-to-reset_reset_n>),       //        reset.reset_n
 		.sdram_addr          (<connected-to-sdram_addr>),          //        sdram.addr
 		.sdram_ba            (<connected-to-sdram_ba>),            //             .ba
@@ -20,9 +24,9 @@
 		.sdram_ras_n         (<connected-to-sdram_ras_n>),         //             .ras_n
 		.sdram_we_n          (<connected-to-sdram_we_n>),          //             .we_n
 		.switches_export     (<connected-to-switches_export>),     //     switches.export
-		.pixel_export        (<connected-to-pixel_export>),        //        pixel.export
-		.camera_ready_export (<connected-to-camera_ready_export>), // camera_ready.export
-		.img_addr_export     (<connected-to-img_addr_export>),     //     img_addr.export
-		.pio_wren_export     (<connected-to-pio_wren_export>)      //     pio_wren.export
+		.accel_I2C_SDAT      (<connected-to-accel_I2C_SDAT>),      //        accel.I2C_SDAT
+		.accel_I2C_SCLK      (<connected-to-accel_I2C_SCLK>),      //             .I2C_SCLK
+		.accel_G_SENSOR_CS_N (<connected-to-accel_G_SENSOR_CS_N>), //             .G_SENSOR_CS_N
+		.accel_G_SENSOR_INT  (<connected-to-accel_G_SENSOR_INT>)   //             .G_SENSOR_INT
 	);
 
